082a535df05de176253925e9ea117895855a6383
Cristiano Pereira <cristianops2020@gmail.com>
2023-09-24 00:49:38 -0300

    ad

--- Program.cs
+++ Program.cs
@@ -1,8 +1,39 @@
-﻿using fundamentos.Models;
+﻿using System.Data.Common;
+using System.Net.Mail;
+using fundamentos.Models;
 
-Pessoa p = new()
-{
-    Nome = "Cristiano",
-    Idade = 40
-};
-p.Apresentar();
+int a = 10;
+int b = 5;
+int c = a + b;
+Console.WriteLine(c);
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual.ToString("dd/mm/yyyy hh:mm"));
+// Console.WriteLine(dataAtual.AddHours(4).ToString("dd/mm/yyyy hh:mm"));
+// Console.WriteLine(dataAtual.AddDays(5).ToString("dd/mm/yyyy"));
+
+// Pessoa p = new()
+// {
+//     Nome = "Cristiano",
+//     Idade = 40
+// };
+// p.Apresentar();
+
+// string apresentacao = "Olá, seja bem vindo!";
+// int quantidade = 10;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preço: " + preco);
+// Console.WriteLine("Valor da variável condiçao: " + condicao);
