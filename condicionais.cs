26a5e4cc803c8f146da15b2ce943e0598af12179
Cristiano Pereira <cristianops2020@gmail.com>
2023-09-25 22:42:58 -0300

    ad

---
+++ condicionais.cs
@@ -0,0 +1,28 @@
+using System.Linq.Expressions;
+using fundamentos.Models;
+
+ 
+
+
+
+
+
+
+/*
+int quantidadeEstoque = 5;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (quantidadeCompra == 0){
+    Console.WriteLine("Venda Inválida!");
+}else if (possivelVenda){
+    Console.WriteLine("Venda Realizada!");
+}else{
+    Console.WriteLine("Desculpe. Estoque esgotado.");
+}
+Console.WriteLine($"Estoque Disponível: {quantidadeEstoque - quantidadeCompra}");
+*/
