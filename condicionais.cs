eb2b1bad0d89fbd43d1478eed0d94071d31ba8ae
Cristiano Pereira <cristianops2020@gmail.com>
2023-09-25 22:56:42 -0300

    ad

--- condicionais.cs
+++ condicionais.cs
@@ -1,7 +1,21 @@
 using System.Linq.Expressions;
 using fundamentos.Models;
 
- 
+ Console.WriteLine("Digite uma letra");
+ string? letra = Console.ReadLine();
+
+ switch(letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    default:
+        Console.WriteLine("Não é uma Vogal");
+        break;
+ }
 
 
 
